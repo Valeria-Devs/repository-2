4bd84b29b38e738324c9229a119996ea63c15d7c
valeria <valeria.valle.shz@gmail.com>
2021-01-30 23:47:09 -0600

    versionobjeto

---
+++ exercice2Funtion.js
@@ -0,0 +1,34 @@
+let total = prompt("¿Cuantos alumnos son?");
+let studens = [];
+for (i = 0; i < total; i++) {
+    studens[i] = [prompt("Nombre del alumno " + (i + 1)), 0];
+
+}
+const assistence = (name, p) => {
+    let assit = promt(name);
+
+    if (assit == "p" || assit == "P") {
+        studens[i][1]++;
+    }
+
+}
+
+for (i = 0; i < 30; i++) {
+    for (student in studens) {
+        assistence(studens[student][0], student);
+    }
+    for (student in studens) {
+        let resp = `${studens [student][0]} : <br>
+________________ Presentes : ${studens[student][1]}
+<br>
+________________ Ausencias : ${30-studens[student][1]}
+`;
+        if (30 > students[student][1]) {
+            resp += `<b style='color:red'> Reprobado por inasistencias </b> <br><br>`;
+
+        } else {
+            resp = `<br><br>`;
+        }
+
+    }
+}
