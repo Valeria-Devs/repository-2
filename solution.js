f32204bf573805f83866a6267cefe39dc7b2a454
valeria <valeria.valle.shz@gmail.com>
2021-01-30 20:42:11 -0600

    versionbucles

--- solution.js
+++ solution.js
@@ -3,39 +3,27 @@ moneyRoberto = prompt("Ingresa el monto que tengas disponible Roberto :");
 moneyPedro = prompt("Ingresa el monto que tengas disponible Pedro :");
 var iceCost = [{ "iceNaem": "agua", "cost": 0.6 }, { "iceNaem": "crema", "cost": 1 }, { "iceNaem": "heladiz", "cost": 1.6 }, { "iceNaem": "heladovich", "cost": 1.7 }, { "iceNaem": "heladrdo", "cost": 1.8 }, { "iceNaem": "confites", "cost": 2.9 }, { "iceNaem": "pote", "cost": 2.9 }];
 
-
-function payIce(name, money) {
-    let money = money;
-    let name = name;
-    let costMax = 0;
+function payIce(moneyCofla) {
+    var money = moneyCofla;
+    let costMax = money;
     let before;
-    var optionIce = [];
     let cash = 0;
     let nameOption;
     for (var i = 0; i < iceCost.length; i++) {
         before = costMax;
-        if (costMax > iceCost[i]['cost']) {
-            costMax = costMax;
+        if (before > iceCost[i]['cost']) {
+            //   nameOption = iceCost[i - 1]['iceNaem'];
             nameOption = iceCost[i]['iceNaem'];
-            cash = money % iceCost[i]['cost'];
-            if (before === iceCost[i]['cost']) {
-                optionIce.push(iceCost[i - 1]);
-                optionIce.push(iceCost[i]);
-                cash = money % iceCost[i]['cost'];
-            }
+            cash = (money - (iceCost[i]['cost']));
+        } else if (before === iceCost[i]['cost']) {
+            nameOption = iceCost[i - 1]['iceNaem'] + iceCost[i]['iceNaem'];
+            cash = (money - (iceCost[i]['cost']));
         } else {
-            costMax = iceCost[i]['cost'];
-            nameOption = iceCost[i]['iceNaem'];
-            cash = money % iceCost[i]['cost'];
-            if (before === iceCost[i]['cost']) {
-                optionIce.push(iceCost[i - 1]);
-                optionIce.push(iceCost[i]);
-                cash = money % iceCost[i]['cost'];
-            }
-        }
-    }
-    console.table(name + 'tiene ' + money + cash);
 
+        }
+
+    }
+    console.log('Con : ' + money + ' puedes comprar  ' + nameOption + ' tu cambio es ' + cash);
 }
 
-payIce('cofla', moneyCofla);
+payIce(parseInt(moneyCofla));
